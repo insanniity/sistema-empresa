adfae93868afa5455d068065e7fb9d1655fa970b
insannity <32269793+insanniity@users.noreply.github.com>
2021-06-14 10:55:16 -0300

    Start Backend

---
+++ backend/src/main/java/com/insannity/apiempresa/ApiEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.insannity.apiempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmpresaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/insannity/apiempresa/ApiEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insannity.apiempresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
