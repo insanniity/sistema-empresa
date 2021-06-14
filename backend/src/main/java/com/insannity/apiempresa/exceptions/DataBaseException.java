48115b936c8d71e54843958b7ef5966d646e732b
insannity <32269793+insanniity@users.noreply.github.com>
2021-06-14 20:28:17 -0300

    Updated Exceptions

---
+++ backend/src/main/java/com/insannity/apiempresa/exceptions/DataBaseException.java
@@ -0,0 +1,10 @@
+package com.insannity.apiempresa.exceptions;
+
+public class DataBaseException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+
+}
--- backend/src/main/java/com/insannity/apiempresa/exceptions/handler/ResourceExceptionHandler.java
+++ backend/src/main/java/com/insannity/apiempresa/exceptions/handler/ResourceExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.insannity.apiempresa.exceptions.handler;
 
 import com.insannity.apiempresa.entities.StandardError;
+import com.insannity.apiempresa.exceptions.DataBaseException;
 import com.insannity.apiempresa.exceptions.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,18 @@ public class ResourceExceptionHandler {
         err.setTimestamp(Instant.now());
         err.setStatus(status.value());
         err.setError("Resource Not found");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Data base exception.");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
         return ResponseEntity.status(status).body(err);
