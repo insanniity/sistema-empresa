d2890915e3299e4ab6a3c57849e6c24f55142dbf
insannity <32269793+insanniity@users.noreply.github.com>
2021-06-14 12:26:45 -0300

    Update Repository Anotation

--- backend/src/main/java/com/insannity/apiempresa/repositories/CollaboratorRepository.java
+++ backend/src/main/java/com/insannity/apiempresa/repositories/CollaboratorRepository.java
@@ -2,6 +2,8 @@ package com.insannity.apiempresa.repositories;
 
 import com.insannity.apiempresa.entities.Collaborator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CollaboratorRepository extends JpaRepository<Collaborator, Long> {
 }
