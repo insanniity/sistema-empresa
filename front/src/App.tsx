626f2852b404209750ee3b4dbbd5e7b7838a8536
insannity <32269793+insanniity@users.noreply.github.com>
2021-06-16 22:17:32 -0300

    front start

---
+++ front/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './core/assets/styles/styles.scss';
+import './App.scss';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
