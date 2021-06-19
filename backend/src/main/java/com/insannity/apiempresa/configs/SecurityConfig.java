2b9444bf1820f31300d0e1af3267a3c143e855fb
insannity <32269793+insanniity@users.noreply.github.com>
2021-06-19 20:05:16 -0300

    Update SecurityConfig.java

--- backend/src/main/java/com/insannity/apiempresa/configs/SecurityConfig.java
+++ backend/src/main/java/com/insannity/apiempresa/configs/SecurityConfig.java
@@ -3,30 +3,17 @@ package com.insannity.apiempresa.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private Environment env;
-
-    private static final String[] PUBLIC_MATCHERS = { "/**", "/h2-console", "/h2-console/*" };
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -42,8 +29,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/actuator/**");
-        web.ignoring().antMatchers("/h2-console","/h2-console/**");
-        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**", "/swagger-ui.html","/swagger-ui/**", "/webjars/**");
+        web.ignoring().antMatchers("/h2-console/**");
+        web.ignoring().antMatchers("/", "/login**");
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**", "/swagger**", "/swagger-ui/**", "/webjars/**");
     }
 
     @Override
@@ -52,25 +40,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManager();
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//        // H2
-//        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-//            http.headers().frameOptions().disable();
-//        }
-//
-//        http.cors().and().csrf().disable();
-//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll();
-//    }
-
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    public UserDetailsService userDetailsService() {
+        return super.userDetailsService();
     }
+
 }
